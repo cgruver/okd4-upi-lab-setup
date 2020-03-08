c64d6eb8fc1c705085ed021ff9e4e9a442643e14
Charro Gruver <charro_gruver@yahoo.com>
2020-03-08 15:20:06 -0400

    Deploy with fixed IP

--- Provisioning/bin/DeployOkdNodes.sh
+++ Provisioning/bin/DeployOkdNodes.sh
@@ -33,6 +33,23 @@ case $i in
 esac
 done
 
+cd ${OKD4_LAB_PATH}
+
+mkdir -p ${OKD4_LAB_PATH}/okd-release-tmp
+cd ${OKD4_LAB_PATH}/okd4-lab/okd-release-tmp
+oc adm release extract --command='openshift-install' ${OKD_RELEASE}
+oc adm release extract --command='oc' ${OKD_RELEASE}
+mv -f openshift-install ~/bin
+mv -f oc ~/bin
+cd ..
+rm -rf okd-release-tmp
+
+rm -rf ${OKD4_LAB_PATH}/okd4-install-dir
+mkdir ${OKD4_LAB_PATH}/okd4-install-dir
+cp ${OKD4_LAB_PATH}/install-config-upi.yaml ${OKD4_LAB_PATH}/okd4-install-dir/install-config.yaml
+openshift-install --dir=${OKD4_LAB_PATH}/okd4-install-dir create ignition-configs
+scp -r ${OKD4_LAB_PATH}/okd4-install-dir/*.ign root@${INSTALL_HOST_IP}:${INSTALL_ROOT}/fcos/ignition/
+
 mkdir -p ${OKD4_LAB_PATH}/ipxe-work-dir
 
 for VARS in $(cat ${INVENTORY} | grep -v "#")
