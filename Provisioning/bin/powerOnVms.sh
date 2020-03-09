8f9694ef2d59d000b86363a7f47e345edf7de05a
Charro Gruver <charro.gruver@odfl.com>
2020-03-09 14:31:18 -0400

    remove deprecated files

--- Provisioning/bin/powerOnVms.sh
+++ Provisioning/bin/powerOnVms.sh
@@ -7,19 +7,14 @@ case $i in
     INVENTORY="${i#*=}"
     shift # past argument=value
     ;;
-    -t=*|--type=*)
-    TYPE="${i#*=}"
-    shift # past argument=value
-    ;;
     *)
           # unknown option
     ;;
 esac
 done
 
-for VARS in $(cat ${INVENTORY} | grep -v "#" | grep ${TYPE})
+for VARS in $(cat ${INVENTORY} | grep -v "#")
 do
-	HOST_NODE=$(echo ${VARS} | cut -d',' -f2)
-	HOSTNAME=$(echo ${VARS} | cut -d',' -f3)
-	ssh root@${HOST_NODE}.${LAB_DOMAIN} "virsh start ${HOSTNAME}"
+	VBMC_PORT=$(echo ${VARS} | cut -d',' -f8)
+    ipmitool -I lanplus -H${INSTALL_HOST_IP} -p${VBMC_PORT} -Uadmin -Ppassword chassis power on
 done
