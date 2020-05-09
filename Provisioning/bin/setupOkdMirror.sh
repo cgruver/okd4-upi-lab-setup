a0e77e6bcd95d86ccdc205c872ffdfbc29cd49d7
Charro Gruver <charro_gruver@yahoo.com>
2020-05-09 08:15:02 -0400

    utility for OKD mirror sync

---
+++ Provisioning/bin/setupOkdMirror.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Enable access to registry.svc.ci.openshift.org
+ssh root@${LAB_NAMESERVER} 'sed -i "s|registry.svc.ci.openshift.org|;sinkhole|g" /etc/named/zones/db.sinkhole && systemctl restart named'
+
+# Mirror $OKD_RELEASE
+oc adm -a ${LOCAL_SECRET_JSON} release mirror --from=${OKD_REGISTRY}:${OKD_RELEASE} --to=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY} --to-release-image=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}:${OKD_RELEASE}
+
+# Disable access to registry.svc.ci.openshift.org
+ssh root@${LAB_NAMESERVER} 'sed -i "s|;sinkhole|registry.svc.ci.openshift.org|g" /etc/named/zones/db.sinkhole && systemctl restart named'
